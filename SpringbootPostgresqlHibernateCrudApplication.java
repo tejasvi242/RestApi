6c8af4bf1b640a3dc44299d4d01a0c3859bf343b
tejasvi242 <57031860+tejasvi242@users.noreply.github.com>
2022-02-28 16:42:25 +0530

    Assignment Work
    ..

---
+++ SpringbootPostgresqlHibernateCrudApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresqlHibernateCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPostgresqlHibernateCrudApplication.class, args);
+	}
+
+}
